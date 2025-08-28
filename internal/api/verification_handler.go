80b6b20bebeef285baa06b5cf09f88157c151295
Augusto Peres <augustoapg@gmail.com>
2025-08-28 17:08:33 -0400

    add private get and upsert helper functions in handler

--- internal/api/verification_handler.go
+++ internal/api/verification_handler.go
@@ -21,37 +21,52 @@ func NewVerificationHandler(logger *log.Logger, kvStoreService *services.KVStore
 	}
 }
 
+func (v *VerificationHandler) upsertAndValidateKV(key string, value string) error {
+	kv, err := v.KvStoreService.UpsertKV(key, value)
+	if err != nil {
+		return fmt.Errorf("error creating kv: %v", err)
+	}
+
+	if kv.Key != key || kv.Value != value {
+		return fmt.Errorf("key or value mismatch when creating kv. Expected: %+v, Got: %+v", services.KV{Key: key, Value: value}, kv)
+	}
+
+	return nil
+}
+
+func (v *VerificationHandler) getAndValidateKV(key string, expectedValue string) error {
+	kv, err := v.KvStoreService.GetKV(key)
+	if err != nil {
+		return fmt.Errorf("error retrieving kv: %v", err)
+	}
+
+	if kv.Key != key || kv.Value != expectedValue {
+		return fmt.Errorf("key or value mismatch when retrieving kv. Expected: %+v, Got: %+v", services.KV{Key: key, Value: expectedValue}, kv)
+	}
+
+	return nil
+}
+
 // VerifyDeletion is a handler that verifies if deleting key-value pairs from kv-store is working correctly.
 // It creates a key-value pair, retrieves it, deletes it, and then attempts to retrieve it again.
 // Returns 200 with success message if successful, or 500 with error message if not.
 func (v *VerificationHandler) VerifyDeletion(w http.ResponseWriter, r *http.Request) {
-	// 1. Create a key-value pair
 	key := "test_key"
 	value := "test_value"
 
-	kv, err := v.KvStoreService.UpsertKV(key, value)
+	// 1. Create a key-value pair
+	err := v.upsertAndValidateKV(key, value)
 	if err != nil {
-		v.Logger.Printf("[VerifyDeletion] error creating kv: %v", err)
-		utils.WriteResponse(w, http.StatusInternalServerError, fmt.Sprintf("error creating kv: %v", err))
-		return
-	}
-
-	if kv.Key != key || kv.Value != value {
-		v.Logger.Printf("[VerifyDeletion] key or value mismatch when creating kv. Expected: %+v, Got: %+v", services.KV{Key: key, Value: value}, kv)
-		utils.WriteResponse(w, http.StatusInternalServerError, fmt.Sprintf("key or value mismatch when creating kv. Expected: %+v, Got: %+v", services.KV{Key: key, Value: value}, kv))
+		v.Logger.Printf("[VerifyDeletion] %v", err)
+		utils.WriteResponse(w, http.StatusInternalServerError, fmt.Sprintf("%v", err))
 		return
 	}
 
 	// 2. Retrieve kv from store to make sure upsert worked
-	kv, err = v.KvStoreService.GetKV(key)
+	err = v.getAndValidateKV(key, value)
 	if err != nil {
-		v.Logger.Printf("[VerifyDeletion] error retrieving kv: %v", err)
-		utils.WriteResponse(w, http.StatusInternalServerError, fmt.Sprintf("error retrieving kv: %v", err))
-	}
-
-	if kv.Key != key || kv.Value != value {
-		v.Logger.Printf("[VerifyDeletion] key or value mismatch when retrieving kv. Expected: %+v, Got: %+v", services.KV{Key: key, Value: value}, kv)
-		utils.WriteResponse(w, http.StatusInternalServerError, fmt.Sprintf("key or value mismatch when retrieving kv. Expected: %+v, Got: %+v", services.KV{Key: key, Value: value}, kv))
+		v.Logger.Printf("[VerifyDeletion] %v", err)
+		utils.WriteResponse(w, http.StatusInternalServerError, fmt.Sprintf("%v", err))
 		return
 	}
 
@@ -64,7 +79,7 @@ func (v *VerificationHandler) VerifyDeletion(w http.ResponseWriter, r *http.Requ
 	}
 
 	// 4. Attempt to retrieve kv from store to make sure delete worked
-	kv, err = v.KvStoreService.GetKV(key)
+	kv, err := v.KvStoreService.GetKV(key)
 	if err == nil || kv.Key != "" || kv.Value != "" {
 		v.Logger.Printf("[VerifyDeletion] kv not deleted. Kv retrieved: %+v", kv)
 		utils.WriteResponse(w, http.StatusInternalServerError, fmt.Sprintf("kv not deleted. Kv retrieved: %+v", kv))
@@ -78,63 +93,39 @@ func (v *VerificationHandler) VerifyDeletion(w http.ResponseWriter, r *http.Requ
 // It creates a key-value pair, retrieves it, overwrites it, and then retrieves it again to make sure the overwrite worked.
 // Returns 200 with success message if successful, or 500 with error message if not.
 func (v *VerificationHandler) VerifyOverwrite(w http.ResponseWriter, r *http.Request) {
-	// 1. Create a key-value pair
 	key := "test_key"
 	value := "test_value"
 
-	kv, err := v.KvStoreService.UpsertKV(key, value)
+	// 1. Create a key-value pair
+	err := v.upsertAndValidateKV(key, value)
 	if err != nil {
-		v.Logger.Printf("[VerifyOverwrite] error creating kv: %v", err)
-		utils.WriteResponse(w, http.StatusInternalServerError, fmt.Sprintf("error creating kv: %v", err))
-		return
-	}
-
-	if kv.Key != key || kv.Value != value {
-		v.Logger.Printf("[VerifyOverwrite] key or value mismatch when creating kv. Expected: %+v, Got: %+v", services.KV{Key: key, Value: value}, kv)
-		utils.WriteResponse(w, http.StatusInternalServerError, fmt.Sprintf("key or value mismatch when creating kv. Expected: %+v, Got: %+v", services.KV{Key: key, Value: value}, kv))
+		v.Logger.Printf("[VerifyOverwrite] %v", err)
+		utils.WriteResponse(w, http.StatusInternalServerError, fmt.Sprintf("%v", err))
 		return
 	}
 
 	// 2. Retrieve kv from store to make sure upsert worked
-	kv, err = v.KvStoreService.GetKV(key)
+	err = v.getAndValidateKV(key, value)
 	if err != nil {
-		v.Logger.Printf("[VerifyOverwrite] error retrieving kv: %v", err)
-		utils.WriteResponse(w, http.StatusInternalServerError, fmt.Sprintf("error retrieving kv: %v", err))
-		return
-	}
-
-	if kv.Key != key || kv.Value != value {
-		v.Logger.Printf("[VerifyOverwrite] key or value mismatch when retrieving kv. Expected: %+v, Got: %+v", services.KV{Key: key, Value: value}, kv)
-		utils.WriteResponse(w, http.StatusInternalServerError, fmt.Sprintf("key or value mismatch when retrieving kv. Expected: %+v, Got: %+v", services.KV{Key: key, Value: value}, kv))
+		v.Logger.Printf("[VerifyOverwrite] %v", err)
+		utils.WriteResponse(w, http.StatusInternalServerError, fmt.Sprintf("%v", err))
 		return
 	}
 
 	// 3. Overwrite the kv
 	value = "test_value_2"
-	kv, err = v.KvStoreService.UpsertKV(key, value)
+	err = v.upsertAndValidateKV(key, value)
 	if err != nil {
-		v.Logger.Printf("[VerifyOverwrite] error overwriting kv: %v", err)
-		utils.WriteResponse(w, http.StatusInternalServerError, fmt.Sprintf("error overwriting kv: %v", err))
-		return
-	}
-
-	if kv.Key != key || kv.Value != value {
-		v.Logger.Printf("[VerifyOverwrite] key or value mismatch when overwriting kv. Expected: %+v, Got: %+v", services.KV{Key: key, Value: value}, kv)
-		utils.WriteResponse(w, http.StatusInternalServerError, fmt.Sprintf("key or value mismatch when overwriting kv. Expected: %+v, Got: %+v", services.KV{Key: key, Value: value}, kv))
+		v.Logger.Printf("[VerifyOverwrite] %v", err)
+		utils.WriteResponse(w, http.StatusInternalServerError, fmt.Sprintf("%v", err))
 		return
 	}
 
 	// 4. Retrieve kv from store to make sure overwrite worked
-	kv, err = v.KvStoreService.GetKV(key)
+	err = v.getAndValidateKV(key, value)
 	if err != nil {
-		v.Logger.Printf("[VerifyOverwrite] error retrieving kv: %v", err)
-		utils.WriteResponse(w, http.StatusInternalServerError, fmt.Sprintf("error retrieving kv: %v", err))
-		return
-	}
-
-	if kv.Key != key || kv.Value != value {
-		v.Logger.Printf("[VerifyOverwrite] key or value mismatch when retrieving kv. Expected: %+v, Got: %+v", services.KV{Key: key, Value: value}, kv)
-		utils.WriteResponse(w, http.StatusInternalServerError, fmt.Sprintf("key or value mismatch when retrieving kv. Expected: %+v, Got: %+v", services.KV{Key: key, Value: value}, kv))
+		v.Logger.Printf("[VerifyOverwrite] %v", err)
+		utils.WriteResponse(w, http.StatusInternalServerError, fmt.Sprintf("%v", err))
 		return
 	}
 
